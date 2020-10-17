6471a1297d885f36c969768997f1a45c2399c4b0
vrlnx <vrlinux@pm.me>
2020-10-17 20:09:48 +0200

    update

--- start-byob.sh
+++ start-byob.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 cd /opt/byob/byob/web-gui
+echo "When it's running it's available in browser"
+echo "$HOSTNAME"".local:5000"
 sudo sh /opt/byob/byob/web-gui/startup.sh > ~/bootspool.log
