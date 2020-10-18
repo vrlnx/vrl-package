781b7e73a73ad4dd54e5f09cee0234a7a5fd9010
vrlnx <vrlinux@pm.me>
2020-10-18 20:31:12 +0200

    update

--- start-byob.sh
+++ start-byob.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 PATH=$PATH:~/.local/bin
 cd ~/byob/web-gui
-echo "Open Chrome or Firefox: ""$HOSTNAME"".local:5000"
+echo "When the server is running open Chrome or Firefox: "
+echo "URL in browser: ""$HOSTNAME"".local:5000"
 echo "Please wait about 3-5 min before the server is started up..."
 ./startup.sh > ~/bootspool.log
