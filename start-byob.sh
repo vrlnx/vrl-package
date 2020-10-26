e488a3b94eb45e56f925b0adfe499699c4571224
vrlnx <vrlinux@pm.me>
2020-10-26 02:50:06 +0100

    update

--- start-byob.sh
+++ start-byob.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 cd ~/byob/web-gui
 echo "When the server is running open Chrome or Firefox: "
-echo "URL in browser: ""$HOSTNAME"".local:5000"
-echo "Please wait about 15-30 min before the server is started up..."
-echo "On slower PC's it can be as long as up to 2-3 hours..."
 # sourcing startup.sh in a better fashion
 . ~/byob/web-gui/startup.sh
